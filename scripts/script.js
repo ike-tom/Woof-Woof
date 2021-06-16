48e562846877629146db0fa235bfcf677cf4c8c3
Tom <80857044+ike-tom@users.noreply.github.com>
2021-06-16 22:32:49 +0200

    script added

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-"use strict"
+'use strict'
+
+const menuButton = document.querySelector(".nav__link-menu")
+const menu = document.querySelector(".nav__links")
+
+menuButton.addEventListener('click', function () {
+    if (menu.classList.contains("nav__links--active")) menu.classList.remove("nav__links--active")
+    else menu.classList.add("nav__links--active")
+})
