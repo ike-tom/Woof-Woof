ed4696c4047d2b1aecd0adbf304f647db9ba3557
Tom <80857044+ike-tom@users.noreply.github.com>
2021-06-17 22:39:02 +0200

    header margin adjustments

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,8 @@
 const menuButton = document.querySelector(".nav__link-menu")
 const menu = document.querySelector(".nav__links")
 
-menuButton.addEventListener('click', function () {
+menuButton.addEventListener('click', function (event) {
+    event.preventDefault()
     if (menu.classList.contains("nav__links--active")) menu.classList.remove("nav__links--active")
     else menu.classList.add("nav__links--active")
 })
